6270fd267787539980c4f0368fd7457276e1cd4f
ujinfly <ujinmcfly@gmail.com>
2021-09-19 14:00:14 +0300

    buttons0

--- pipicoce/pipicoce.c
+++ pipicoce/pipicoce.c
@@ -9,10 +9,18 @@
 #include "hardware/i2c.h"
 #include "pico/binary_info.h"
 #include "hardware/clocks.h"
+//
+#include "hardware/gpio.h"
+//KHOnKu
+//#define FIRST_GPIO 2
+const uint8_t BUTTON_L_GPIO =(27);
+const uint8_t BUTTON_R_GPIO =(21);
+const uint8_t BUTTON_U_GPIO =(20);
+const uint8_t BUTTON_D_GPIO =(19);
+const uint8_t BUTTON_OK_GPIO =(18);
+
+
 //KOHCTAHTbI
-
-
-
  const uint8_t SET_CONTRAST =0x81;
  const uint8_t SET_ENTIRE_ON =0xA4;
  const uint8_t SET_NORM_INV =0xA6;
@@ -32,12 +40,39 @@
  const uint8_t SET_CHARGE_PUMP =0x8D;
 const uint8_t SET_DISP_OFFSET =0x8D;
 int countPerSecond = 0;
+const uint LED_PIN = PICO_DEFAULT_LED_PIN;
+
 void CalculateCPS(){
     countPerSecond = 0;
     
 }
-void DisplayPowerON(){
+void InitDisplay(){
+
+    #define PIN_SDA 0
+    #define PIN_SCL 1
+    //
+    gpio_set_function(PIN_SDA, GPIO_FUNC_I2C);
+    gpio_set_function(PIN_SCL, GPIO_FUNC_I2C);
+    gpio_pull_up(PIN_SDA);
+    gpio_pull_up(PIN_SCL);
+    // Make the I2C pins available to picotool
+    bi_decl(bi_2pins_with_func(PIN_SDA, PIN_SCL, GPIO_FUNC_I2C));
+
+    i2c_init(i2c_default, 100 * 1000);
+    //
     
+    gpio_init(LED_PIN);
+    gpio_set_dir(LED_PIN, GPIO_OUT);  
+    gpio_put(LED_PIN, 1);
+    sleep_ms(5);
+    gpio_put(LED_PIN, 0);
+    sleep_ms(5); 
+    DisplayUpdate();
+    gpio_put(LED_PIN, 1);
+    sleep_ms(5);
+    gpio_put(LED_PIN, 0);
+    sleep_ms(5);
+    MoveCursorToZero();
 }
 uint8_t _ws[4];
 
@@ -176,7 +211,7 @@ void Write8x8_clear(int count){
         0b00000000,
     };
     i2c_write_blocking(i2c0,0b0111100,w,sizeof(w)/sizeof(w[0]),false);
-    sleep_ms(10);
+    //sleep_ms(10);
     if(count<=0)return;
     Write8x8_clear(count-1);
 }
@@ -193,7 +228,7 @@ void Write8x8_kresto(int count){
         0b00000000,
     };
     i2c_write_blocking(i2c0,0b0111100,w,sizeof(w)/sizeof(w[0]),false);
-    sleep_ms(10);
+    //sleep_ms(10);
     if(count<=0)return;
     Write8x8_kresto(count-1);
 }
@@ -219,7 +254,7 @@ void DisplayUpdate(){
     bool _power = false;
     //pagebuffer = None
     bool page_column_start = false;
-    sleep_ms(500);
+    sleep_ms(100);
     ///i2c.writeto(0x3c, bytes(SET_DISP | 0x01));
     i2c_write_blocking(i2c0,addr,SET_DISP | 0x01,2,false);
     
@@ -277,43 +312,29 @@ void DisplayUpdate(){
 }
 
 int main() {
-    //stdio_init_all();
-
-    #define PIN_SDA 0
-    #define PIN_SCL 1
-    //
-    gpio_set_function(PIN_SDA, GPIO_FUNC_I2C);
-    gpio_set_function(PIN_SCL, GPIO_FUNC_I2C);
-    gpio_pull_up(PIN_SDA);
-    gpio_pull_up(PIN_SCL);
-    // Make the I2C pins available to picotool
-    bi_decl(bi_2pins_with_func(PIN_SDA, PIN_SCL, GPIO_FUNC_I2C));
-
-    i2c_init(i2c_default, 100 * 1000);
-    //
-    const uint LED_PIN = PICO_DEFAULT_LED_PIN;
-    gpio_init(LED_PIN);
-    gpio_set_dir(LED_PIN, GPIO_OUT);  
-    gpio_put(LED_PIN, 1);
-    sleep_ms(100);
-    gpio_put(LED_PIN, 0);
-    sleep_ms(10); 
-    DisplayUpdate();
-    gpio_put(LED_PIN, 1);
-    sleep_ms(100);
-    gpio_put(LED_PIN, 0);
-    sleep_ms(10);
-    MoveCursorToZero();
     
+    InitDisplay();
+    //кнопки
+    gpio_init(BUTTON_L_GPIO);
+    gpio_set_dir(BUTTON_L_GPIO, GPIO_IN);
+    // We are using the button to pull down to 0v when pressed, so ensure that when
+    // unpressed, it uses internal pull ups. Otherwise when unpressed, the input will
+    // be floating.
+    gpio_pull_up(BUTTON_L_GPIO);
+    gpio_pull_up(BUTTON_R_GPIO);
+    gpio_pull_up(BUTTON_U_GPIO);
+    gpio_pull_up(BUTTON_D_GPIO);
+    gpio_pull_up(BUTTON_OK_GPIO);
+
     while (true) {
         gpio_put(LED_PIN, 1);
-        sleep_ms(50);       
-        //GetSingleNomber(lit) ;
-        //PrintLitera(_ws);
+        sleep_ms(10);       
+        GetSingleNomber(0) ;
+        PrintLitera(_ws);
         
         
         gpio_put(LED_PIN, 0);
-        sleep_ms(50); 
+        sleep_ms(10); 
     }    
 
 }
